ef22b2cd99d2e038ebb62a34fd40f12533e364f8
emylincon <emylincon@gmail.com>
2019-02-16 13:37:33 +0000

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -12,6 +12,14 @@ apt install python3-pyfiglet -y
 apt install python3-sqlite3 -y
 apt install openssh-client -y
 apt install openssh-server -y
+apt install wget -y
+apt install curl -y
+apt install man -y
+apt install apt-utils -y
+apt install iputils-ping -y
+apt install net-tools -y
+apt install nano -y
+apt install iperf3 -y
 mv * ..
 clear
 echo 'All done.. Ready to use!'
