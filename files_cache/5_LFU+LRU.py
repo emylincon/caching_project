62afaaab9e59c024811b56a40ae230c49aba9bcc
Emeka E Ugwuanyi <emylincon@gmail.com>
2019-04-14 16:16:20 +0100

    Update 5_LFU+LRU.py

--- files_cache/5_LFU+LRU.py
+++ files_cache/5_LFU+LRU.py
@@ -381,6 +381,9 @@ def cache_performance():
     print('----------------------------------------------------------')
     print('         Total use of Replacement Algorithm = {}'.format(re_use))
     print('----------------------------------------------------------')
+    cmd = f"echo 'lfru{cache_size}_local_hits = {H} \nlfru{cache_size}_miss = {M} \nlfru{cache_size}_mec_hit = {MH} \n" \
+          f"lfru{cache_size}_total_hit = {H+MH}' >> /home/mec/cache_result.py"
+    os.system(cmd)
 
 
 def getting_ready():
