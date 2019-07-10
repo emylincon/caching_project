dc367f2b0a7d5116bbe88dd02d85935bb663cb3c
Emeka Ugwuanyi <emylincon@gmail.com>
2019-07-10 22:16:27 +0100

    improving . ..

--- files_cache/2.1.1_presentation_proposed_algo_gui.py
+++ files_cache/2.1.1_presentation_proposed_algo_gui.py
@@ -148,7 +148,7 @@ def plot_local_cache_freq():
 def plot_cpu():
     ax2.grid(True)
     cx, cy = scale_axis(y_axis)
-    ax2.plot(cx, cy, linewidth=2, label='CPU', color='b', linestyle='dashed', alpha=0.5)
+    ax2.plot(cx, cy, linewidth=2, label='CPU', linestyle='dashed', alpha=0.5)
     ax2.set_title('CPU Utilization over Time')
     ax2.set_ylabel('URL')
     ax2.fill_between(cx, cy, 0, alpha=0.5)
