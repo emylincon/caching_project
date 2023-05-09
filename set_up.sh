ce97ec7d4785cab88bb94751f4c649e392c5cf36
Emeka E Ugwuanyi <emeka.ugwuanyi@sky.uk>
2023-05-09 08:27:12 +0100

    readme update

--- set_up.sh
+++ set_up.sh
@@ -4,20 +4,20 @@ clear
 echo 'preparing your MEC Caching Platform'
 sleep 3
 apt update && apt upgrade -y
-apt install python3 -y
-apt install python3-pip -y
-apt install zip -y
-apt install openssh-client -y
-apt install openssh-server -y
-apt install wget -y
-apt install curl -y
-apt install man -y
-apt install apt-utils -y
-apt install iputils-ping -y
-apt install net-tools -y
-apt install nano -y
-apt install iperf3 -y
-apt install nmap -y
+apt install -y python3 \
+    python3-pip \
+    zip \
+    openssh-client \
+    openssh-server \
+    wget \
+    curl \
+    man \
+    apt-utils \
+    iputils-ping \
+    net-tools \
+    nano \
+    iperf3 \
+    nmap
 
 pip3 install -r requirements.txt
 
